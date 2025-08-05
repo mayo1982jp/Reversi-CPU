c104bb284887b1e5cbaa095a3f4fd9568901f849
mayo1982jp <mayo810246@gmail.com>
2025-08-06 06:21:33 +0900

    [dyad] Cleanup: remove unused files and unify Reversi logic for easier future extensions - wrote 3 file(s), deleted 4 file(s)

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,4 +1,4 @@
-import { useToast } from "@/hooks/use-toast";
+import { useToast } from "@/components/ui/use-toast";
 import {
   Toast,
   ToastClose,
@@ -18,9 +18,7 @@ export function Toaster() {
           <Toast key={id} {...props}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
+              {description && <ToastDescription>{description}</ToastDescription>}
             </div>
             {action}
             <ToastClose />
